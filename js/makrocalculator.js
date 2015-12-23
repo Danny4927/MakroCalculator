e52b5bb3964f5320b16f20783d4d648a7790fbb6
Danny <danielknoell@gmx.de>
2015-12-23 16:23:07 +0100

    show RMR as alert

---
+++ js/makrocalculator.js
@@ -0,0 +1,55 @@
+
+
+
+  var calcRMR = function () {
+    var elemMale = document.getElementById("gridRadios1");
+    var elemFemale = document.getElementById("gridRadios2");
+    var ageGroup1 = document.getElementById("rAge1");
+    var ageGroup2 = document.getElementById("rAge2");
+    var ageGroup3 = document.getElementById("rAge3");
+    var ageGroup4 = document.getElementById("rAge4");
+    var rmr = "Angaben fehlen";
+
+    //male
+	  if (elemMale.checked){
+		  if (ageGroup1.checked){
+        rmr = calcRMRValues(17.5 , 651);
+      }
+      if (ageGroup2.checked){
+        rmr = calcRMRValues(15.3 , 679);
+      }
+      if (ageGroup3.checked){
+        rmr = calcRMRValues(11.6 , 879);
+      }
+      if (ageGroup4.checked){
+        rmr = calcRMRValues(13.5 , 487);
+      }
+    }
+
+    //female
+    if (elemFemale.checked){
+      if (ageGroup1.checked){
+        rmr = calcRMRValues(12.2 , 749);
+      }
+      if (ageGroup2.checked){
+        rmr = calcRMRValues(14.7 , 496);
+      }
+      if (ageGroup3.checked){
+        rmr = calcRMRValues(8.7 , 829);
+      }
+      if (ageGroup4.checked){
+        rmr = calcRMRValues(10.5 , 596);
+      }
+    }
+    alert(rmr);
+    return rmr;
+
+  };
+
+  var calcRMRValues = function (faktor, summand) {
+    //alert(summand);
+    var weight = document.getElementById("weight");
+    var produkt = faktor * weight.value;
+    var rmr = produkt + summand;
+    return rmr;
+  };
